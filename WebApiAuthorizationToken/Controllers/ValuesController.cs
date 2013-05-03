c45e8a802fac5cf68b9840c3d062f7f1a5a7dcc1
devcurry <devcurry@a2zknowledgevisuals.com>
2013-05-03 09:05:45 +0530

    Code Commit

---
+++ WebApiAuthorizationToken/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Helpers;
+using System.Web.Http;
+
+namespace WebApiAuthorizationToken.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [Authorize]
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            CookieHeaderValue cookie = Request.Headers
+                .GetCookies(AntiForgeryConfig.CookieName)
+                .FirstOrDefault();
+            if (cookie != null)
+            {
+                Stream requestBufferedStream = Request.Content.ReadAsStreamAsync().Result;
+                requestBufferedStream.Position = 0;
+                NameValueCollection myform = Request.Content.ReadAsFormDataAsync().Result;
+                try
+                {
+                    AntiForgery.Validate(cookie[AntiForgeryConfig.CookieName].Value, myform[AntiForgeryConfig.CookieName]);
+                }
+                catch (Exception ex)
+                {
+                    throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.Unauthorized));
+                }
+            }
+            else
+            {
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.Unauthorized));
+            }
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApiAuthorizationToken/Handlers/AntiForgeryTokenHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Http;
+
+namespace WebApiAuthorizationToken.Handlers
+{
+    public class AntiForgeryTokenHandler : DelegatingHandler
+    {
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, 
+            CancellationToken cancellationToken)
+        {
+            bool isCsrf = true;
+            CookieHeaderValue cookie = request.Headers
+                                          .GetCookies(AntiForgeryConfig.CookieName)
+                                          .FirstOrDefault();
+            if (cookie != null)
+            {
+                Stream requestBufferedStream = request.Content.ReadAsStreamAsync().Result;
+                requestBufferedStream.Position = 0;
+                NameValueCollection myform = request.Content.ReadAsFormDataAsync().Result;
+                requestBufferedStream.Position = 0;
+                try
+                {
+                    AntiForgery.Validate(cookie[AntiForgeryConfig.CookieName].Value,
+                     myform[AntiForgeryConfig.CookieName]);
+                    isCsrf = false;
+                }
+                catch (Exception ex)
+                {
+                    return request.CreateResponse(HttpStatusCode.Forbidden);
+                }
+            }
+            if (isCsrf)
+            {
+                return request.CreateResponse(HttpStatusCode.Forbidden);
+            }
+            
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
